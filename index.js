ea84984b1fa02900261a42b2997e95cc1db2ad37
wolfscatt <bjkeses11@hotmail.com>
2023-09-26 21:37:36 +0300

    error handling

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var bodyParser = require("body-parser")
 const db = require('./services/connectionDb')
 const productService = require('./services/productService')
 const userService = require('./services/userService')
-const mainService = require("./services/mainService")
 
 const cors = require('cors')
 
@@ -13,9 +12,8 @@ app.use(cors())
 app.use(bodyParser.json())
 db()
 
-app.use('/api', mainService.router)
-app.use('/api/products', productService.router)
-app.use('/api/admin', userService.router)
+app.use('/products', productService.router)
+app.use('/admin', userService.router)
 
 app.listen(process.env.PORT || 3000)
 
--- services/mainService.js
+++
@@ -1,9 +0,0 @@
-var router = require('../routes/routes')
-
-router.get("/", (req, res) => {
-    res.status(200).json({ server: "up" });
-  });
-
-
-const main = {router}
-module.exports = main
